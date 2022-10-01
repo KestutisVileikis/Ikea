239b5b0c0ac042afe130864b639ad45e290ccba7
FA PC <kestas.giftika@gmail.com>
2022-10-01 18:15:53 +0300

    Build and create in IkeaBusinessPage.java

--- src/test/java/lt/kestutis/pom/pages/ikea/IkeaBusinessPage.java
+++ src/test/java/lt/kestutis/pom/pages/ikea/IkeaBusinessPage.java
@@ -2,10 +2,10 @@ package lt.kestutis.pom.pages.ikea;
 
 public class IkeaBusinessPage {
     public static void clickButtonAcceptCookies() {
-Common.clickElement(Locators.IkeaHome.clickWebCookies);
+        Common.clickElement(Locators.IkeaHome.clickWebCookies);
     }
-    public static
-    void clickButtonPlaceTheOrder() {
+
+    public static void clickButtonPlaceTheOrder() {
         Common.clickElement(Locators.IkeaBusiness.clickButtonPlaceTheOrder);
     }
 
@@ -13,9 +13,11 @@ Common.clickElement(Locators.IkeaHome.clickWebCookies);
     public static void enterProductCode(String productCode) {
         Common.sendKeysToElement(Locators.IkeaBusiness.enterProductCode, productCode);
     }
+
     public static void clickButtonAddItem() {
-Common.clickElement(Locators.IkeaBusiness.clickButtonAddItem);
+        Common.clickElement(Locators.IkeaBusiness.clickButtonAddItem);
     }
+
     public static void enterNameOfTheRepresentative(String nameOfTheRepresentative) {
         Common.sendKeysToElement(Locators.IkeaBusiness.enterPersonName, nameOfTheRepresentative);
     }
@@ -27,18 +29,45 @@ Common.clickElement(Locators.IkeaBusiness.clickButtonAddItem);
     public static void enterCompanyName(String companyName) {
         Common.sendKeysToElement(Locators.IkeaBusiness.inputCompanyName, companyName);
     }
+
     public static void enterCompanyAddress(String companyAddress) {
         Common.sendKeysToElement(Locators.IkeaBusiness.inputCompanyAddress, companyAddress);
     }
+
     public static void enterEmail(String email) {
         Common.sendKeysToElement(Locators.IkeaBusiness.inputEmailAddress, email);
     }
+
     public static void enterCompanyCode(String companyCode) {
         Common.sendKeysToElement(Locators.IkeaBusiness.inputCompanyCode, companyCode);
     }
 
     public static void enterTelephoneNumber(String telephoneNumber) {
         Common.sendKeysToElement(Locators.IkeaBusiness.inputTelephoneNumber, telephoneNumber);
+    }
+
+    public static void clickTermsAndConditions() {
+        Common.clickElement(Locators.IkeaBusiness.clickCheckBoxTermsAndConditions);
+    }
+
+    public static void clickMarketingNewsletter() {
+        Common.clickElement(Locators.IkeaBusiness.clickCheckBoxMarketing);
+    }
+
+    public static void clickButtonAssembly() {
+        Common.clickElement(Locators.IkeaBusiness.clickButtonAssemblyServices);
+    }
+
+    public static void clickButtonDelivery() {
+        Common.clickElement(Locators.IkeaBusiness.clickDeliveryOption);
+    }
+
+    public static void clickButtonSubmit() {
+        Common.clickElement(Locators.IkeaBusiness.clickSubmit);
+    }
+
+    public static void readRegistrationText() {
+        Common.getElementText(Locators.IkeaBusiness.readRegistrationText);
     }
 }
 
