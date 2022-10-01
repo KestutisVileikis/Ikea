4ed16b9bfb60460a22eb0e9d782cc0d52349dd5a
FA PC <kestas.giftika@gmail.com>
2022-10-01 17:38:13 +0300

    Build and create in IkeaBusinessTest.java

---
+++ src/test/java/lt/kestutis/pom/pages/test/ikea/IkeaBusinessTest.java
@@ -0,0 +1,45 @@
+package lt.kestutis.pom.pages.test.ikea;
+
+import lt.kestutis.pom.pages.ikea.IkeaBusinessPage;
+import lt.kestutis.pom.pages.ikea.IkeaHomePages;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class IkeaBusinessTest extends TestBase {
+    @BeforeTest
+
+    public void setUp() {
+
+        IkeaHomePages.open();
+        IkeaBusinessPage.clickButtonAcceptCookies();
+        IkeaHomePages.clickElementPage();
+    }
+
+    @Test
+    public void testIkeaBusinessRegistration() {
+
+        String productCode = "59281847";
+        String nameOfTheRepresentative = "Kestutis";
+        String surnameOfTheRepresentative = "Vileikis";
+        String companyName = "Rojaus virtuve";
+        String companyAddress = "Dvaro st. 135, Siauliai";
+        String email = "yes@exportgateway.eu";
+        String companyCode = "303401364";
+        String telephoneNumber = "+37069851148";
+
+
+        IkeaBusinessPage.clickButtonPlaceTheOrder();
+        IkeaBusinessPage.enterProductCode(productCode);
+        IkeaBusinessPage.clickButtonAddItem();
+
+        IkeaBusinessPage.enterNameOfTheRepresentative(nameOfTheRepresentative);
+        IkeaBusinessPage.enterSurnameOfTheRepresentative(surnameOfTheRepresentative);
+        IkeaBusinessPage.enterCompanyName(companyName);
+        IkeaBusinessPage.enterCompanyAddress(companyAddress);
+        IkeaBusinessPage.enterEmail(email);
+        IkeaBusinessPage.enterCompanyCode(companyCode);
+        IkeaBusinessPage.enterTelephoneNumber(telephoneNumber);
+
+        }
+    }
+
